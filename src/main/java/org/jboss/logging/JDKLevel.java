6fc440c45e530277b4869412ab67cee73ca66125
Andrew Nepogoda <andrew.nepogoda@gmail.com>
2018-06-22 15:01:52 +0300

    remove redundant logging level

--- src/main/java/org/jboss/logging/JDKLevel.java
+++ src/main/java/org/jboss/logging/JDKLevel.java
@@ -38,8 +38,6 @@ final class JDKLevel extends Level {
     public static final JDKLevel FATAL = new JDKLevel("FATAL", 1100);
     public static final JDKLevel ERROR = new JDKLevel("ERROR", 1000);
     public static final JDKLevel WARN = new JDKLevel("WARN", 900);
-    @SuppressWarnings("hiding")
-    public static final JDKLevel INFO = new JDKLevel("INFO", 800);
     public static final JDKLevel DEBUG = new JDKLevel("DEBUG", 500);
     public static final JDKLevel TRACE = new JDKLevel("TRACE", 400);
 }
